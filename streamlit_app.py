d6fcd7e8cd13b36c0e79f07a493bedb1acfb9500
ikenurcahya740-commits <ikenurcahya740@gmail.com>
2026-05-22 14:20:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ st.write(
 )
 st.title("ike cahya's project")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+import datetime
+import streamlit as st
+
+d = st.date_input("When's your birthday", datetime.date(2007, 02, 25))
+st.write("my birthday is:", d)
