aae17cd909a7a7a0cc071ebb37cfc2716d32661a
ikenurcahya740-commits <ikenurcahya740@gmail.com>
2026-05-22 14:16:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title("🎈 ike cahya's project")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.title("ke cahya's project")
+st.title("ike cahya's project")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
